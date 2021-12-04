151341396e65b58e04ae87ff5ce32804de7b90fb
Navimaurya <mauryanavis@gmail.com>
2021-12-04 17:29:11 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const rowCol = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
+  const [checked, setChecked] = useState([]);
+  const setBox = (index) => {
+    if (checked.length > 15 || checked.includes(index)) {
+      return
+    }
+    setChecked([...checked, index])
+    console.log(checked);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container pt-5">
+        <div>
+          {rowCol.map((row, i) => (
+            <div className='row' key={i}>
+              {row.map((col, j) => (
+                <div onClick={() => { setBox(col) }} className={`col border border-dark ${checked.length <= 2 && checked[0] === col ? 'bg-danger' : checked.slice(Math.max(checked.length - 2, 1)).includes(col) ? 'bg-danger' : ""}`} style={{ height: '100px', background: 'blue' }}>
+                  <p>
+                    {checked.includes(col) ? `Box #${col}` : ""}
+                  </p>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
